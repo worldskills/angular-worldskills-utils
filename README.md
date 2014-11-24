d40e6f33d3da89e97a1c8be4f2928413fc4e17c2
Fabian Vogler <fabian@equivalence.ch>
2014-11-24 13:58:10 -0200

    Add spinner directive

--- README.md
+++ README.md
@@ -113,3 +113,15 @@ Add `forbiddenFallbad: function` to `app.js`
   }
 })
 ```
+
+### Spinner directive
+
+Show a loading indicator on a button.
+
+```html
+<button type="submit" class="btn btn-success">
+	Save
+	<ws-spinner ng-show="loading" class="ng-hide"></ws-spinner>
+</button>
+```
+
--- src/angular-worldskills-utils.js
+++ src/angular-worldskills-utils.js
@@ -184,4 +184,25 @@
         }];
     });
 
+    utils.directive('wsSpinner', function($interval) {
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+                var i = 0;
+                $interval(function () {
+                    i = (i + 1) % 4;
+                    var loading = '';
+                    for (var j = 0; j < 3; j++) {
+                        if (j < i) {
+                            loading += '.';
+                        } else {
+                            loading += '&nbsp;';
+                        }
+                    }
+                    element.html(loading);
+                }, 300);
+            }
+        };
+    });
+
 })();
