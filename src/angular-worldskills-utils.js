f616be68743f9348faeb79c54d41ae78fdf74f38
Fabian Vogler <fabian@equivalence.ch>
2015-07-14 16:10:24 +0200

    Remove whitespace

--- src/angular-worldskills-utils.js
+++ src/angular-worldskills-utils.js
@@ -108,11 +108,11 @@
             auth.loggedIn = !!auth.accessToken;
             auth.loginUrl = WORLDSKILLS_AUTHORIZE_URL + '?response_type=token&state=' + encodeURIComponent(oauthState) + '&client_id=' + encodeURIComponent(WORLDSKILLS_CLIENT_ID) + '&redirect_uri=' + encodeURIComponent(appUrl);
 
-            auth.refreshRoles = function(){                
+            auth.refreshRoles = function(){
                 var user = auth.getUser();
                 auth.user = {};
                 auth.user.$promise = user;
-                return auth.user.$promise;                        
+                return auth.user.$promise;
             };
 
             auth.getUser = function(){
